852759aa1b980c0216f3756a4fc114bd6023711d
xukuan <xukuan1@huawei.com>
2022-01-05 20:29:06 +0800

    code commit
    Signed-off-by: xukuan <xukuan1@huawei.com>

--- ressched/services/resschedmgr/test/unittest/include/config_reader_test.h
+++ ressched/services/resschedmgr/test/unittest/include/config_reader_test.h
@@ -1,8 +1,30 @@
-//
-// Created by laizhixian on 2022/1/5.
-//
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ * Description: config reader unnittest
+ */
 
-#ifndef RESOURCESCHEDULE_CONFIG_READER_TEST_H
-#define RESOURCESCHEDULE_CONFIG_READER_TEST_H
 
-#endif //RESOURCESCHEDULE_CONFIG_READER_TEST_H
+#ifndef FOUNDATION_RESOURCESCHEDULE_SERVICES_RESSCHEDMGR_TEST_UNITTEST_INCLUDE_CONFIG_READER_TEST_H
+#define FOUNDATION_RESOURCESCHEDULE_SERVICES_RESSCHEDMGR_TEST_UNITTEST_INCLUDE_CONFIG_READER_TEST_H
+
+#include <map>
+#include <memory>
+#include "gtest/gtest.h"
+#include "config_reader.h"
+
+namespace OHOS {
+namespace ResourceSchedule {
+class ConfigReaderTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+protected:
+    std::shared_ptr<ConfigReader> configReader_;
+
+};
+}// namespace ResourceSchedule
+}// namespace OHOS
+
+#endif //FOUNDATION_RESOURCESCHEDULE_SERVICES_RESSCHEDMGR_TEST_UNITTEST_INCLUDE_CONFIG_READER_TEST_H
--- ressched/services/resschedmgr/test/unittest/src/config_reader_test.cpp
+++ ressched/services/resschedmgr/test/unittest/src/config_reader_test.cpp
@@ -1,6 +1,58 @@
-//
-// Created by laizhixian on 2022/1/5.
-//
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ * Description: config reader unnittest
+ */
 
-#include "config_reader.h"
+#include "config_reader_test.h"
 
+using namespace std;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace ResourceSchedule {
+namespace {
+const string PLUGIN_NAME = "demo";
+const string CONIFG_NAME = "sample";
+const string TEST_PREFIX_RES_PATH = "/data/test/resource/resschedfwk/parseconfig/";
+}
+
+void ConfigReaderTest::SetUpTestCase() {}
+
+void ConfigReaderTest::TearDownTestCase() {}
+
+void ConfigReaderTest::SetUp()
+{
+    /**
+     * @tc.setup: initialize the member variable configReader_
+     */
+    configReader_ = make_shared<ConfigReader>();
+}
+
+void ConfigReaderTest::TearDown()
+{
+    /**
+     * @tc.teardown: clear configReader_
+     */
+    configReader_ = nullptr;
+}
+
+/**
+ * @tc.name: Load Config File 001
+ * @tc.desc: Verify if can load not exist file.
+ * @tc.type: FUNC
+ * @tc.require: AR000001
+ * @tc.author:xukuan
+ */
+HWTEST_F(ConfigReaderTest, LoadConfigFile001, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. load not exist config file
+     * @tc.expected: step1. return false when load not exist file
+     */
+     bool ret = configReader_->LoadFromCustConfigFile("fileNotExist");
+     EXPECT_TRUE(!ret);
+}
+
+}
+
+}
