ff597e1bbdac18d1ebafefbd34feb5015fa2fc01
xukuan <xukuan1@huawei.com>
2022-01-08 14:32:49 +0800

    code commit
    Signed-off-by: xukuan <xukuan1@huawei.com>

--- ressched/services/resschedservice/src/res_sched_service_ability.cpp
+++ ressched/services/resschedservice/src/res_sched_service_ability.cpp
@@ -4,8 +4,8 @@
  */
 
 #include "res_sched_service_ability.h"
-//#include "res_sched_log.h"
-//#include "res_sched_mgr.h"
+#include "res_sched_log.h"
+#include "res_sched_mgr.h"
 #include "system_ability_definition.h"
 
 namespace OHOS {
@@ -15,19 +15,15 @@ REGISTER_SYSTEM_ABILITY_BY_ID(ResSchedServiceAbility, RES_SCHED_SYS_ABILITY_ID, 
 
 void ResSchedServiceAbility::OnStart()
 {
-    //ResSchedMgr::GetInstance().Init();
-    //RESSCHED_LOGI("ResSchedServiceAbility ::OnStart.");
-    int a = 0;
-    a++;
+    ResSchedMgr::GetInstance().Init();
+    RESSCHED_LOGI("ResSchedServiceAbility ::OnStart.");
 }
 
 void ResSchedServiceAbility::OnStop()
 {
-    //ResSchedMgr::GetInstance().Stop();
-    //RESSCHED_LOGI("ResSchedServiceAbility::OnStop!");
-    int b = 2;
-    b++;
+    ResSchedMgr::GetInstance().Stop();
+    RESSCHED_LOGI("ResSchedServiceAbility::OnStop!");
 }
 
 } // namespace ResourceSchedule
-} // namespace OHOS 
+} // namespace OHOS
