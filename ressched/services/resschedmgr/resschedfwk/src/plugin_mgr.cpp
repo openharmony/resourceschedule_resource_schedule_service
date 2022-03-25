0d811283f527d678576ab05f2861f7d5be025ae8
xukuan <xukuan1@huawei.com>
2022-03-25 16:55:41 +0800

    commit
    Signed-off-by: xukuan <xukuan1@huawei.com>

--- ressched/services/resschedmgr/resschedfwk/src/plugin_mgr.cpp
+++ ressched/services/resschedmgr/resschedfwk/src/plugin_mgr.cpp
@@ -39,7 +39,7 @@ namespace {
     const std::string PLUGIN_SWITCH_FILE_NAME = "/system/etc/ressched/res_sched_plugin_switch.xml";
     const std::string CONFIG_FILE_NAME = "/system/etc/ressched/res_sched_config.xml";
     static jmp_buf env;
-    const int SIG_ALL[] = {SIGHUP, SIGINT, SIGQUIT, SIGABRT, SIGBUS, SIGTERM};
+    const int SIG_ALL[] = {SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGABRT, SIGBUS, SIGTERM};
 }
 
 extern "C" void Back(int sig)
