8769ccc17ad1c306d81eed3a9b290f5a1fa08254
nainaiguang <qiunaiguang@huawei.com>
2023-04-19 17:43:37 +0800

    add calling uid for reporter
    Signed-off-by: nainaiguang <qiunaiguang@huawei.com>

--- ressched/services/resschedservice/src/res_sched_service.cpp
+++ ressched/services/resschedservice/src/res_sched_service.cpp
@@ -37,7 +37,7 @@ void ResSchedService::ReportData(uint32_t resType, int64_t value, const nlohmann
     RESSCHED_LOGI("ResSchedService::ReportData from ipc receive data resType = %{public}u, value = %{public}lld.",
                   resType, (long long)value);
     const nlohmann::json* payloadP = &payload;
-    unit32_t callingUid = IPCSkeleton::GetCallingUid();
+    uint32_t callingUid = IPCSkeleton::GetCallingUid();
     nlohmann::json* payloadM = const_cast<nlohmann::json*>(payloadP);
     (*payloadM)["callingUid"] = std::to_string(callingUid);
     ResSchedMgr::GetInstance().ReportData(resType, value, *payloadM);
