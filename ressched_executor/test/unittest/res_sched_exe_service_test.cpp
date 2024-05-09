dd5d106fcc87da643a17077bebae569b95fa5785
unknown <baiheng5@huawei.com>
2024-05-09 21:22:59 +0800

    add
    Signed-off-by: unknown <baiheng5@huawei.com>

--- ressched_executor/test/unittest/res_sched_exe_service_test.cpp
+++ ressched_executor/test/unittest/res_sched_exe_service_test.cpp
@@ -264,7 +264,8 @@ public:
         return 0;
     }
 
-    int32_t ResSchedExeService::KillProcess(pid_t pid) {
+    int32_t ResSchedExeService::KillProcess(pid_t pid)
+    {
         return 0;
     }
 };
