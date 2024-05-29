e3937d7afec8fb0c49a8b382d39d66fa13e06749
yanwen <baiheng5@huawei.com>
2024-05-29 20:33:27 +0800

    add
    Signed-off-by: yanwen <baiheng5@huawei.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -222,9 +222,9 @@ enum : uint32_t {
     RES_TYPE_BOOT_COMPLETED = 94,
     // report continuous application startup
     RES_TYPE_CONTINUOUS_STARTUP = 95,
-    //report account activating
+    // report account activating
     RES_TYPE_ACCOUNT_ACTIVATING = 96,
-    //last resType
+    // last resType
     RES_TYPE_LAST,
 };
 
