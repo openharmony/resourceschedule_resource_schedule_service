2c9d6cc9e66149a3fc63b9800fc476af60f2b05b
g00857378 <guochenyang@huawei.com>
2024-06-14 17:15:40 +0800

    Revert "新增app cached状态的感知和消息转发"
    This reverts commit 3675666d7d9d45d20e104ab585a35308eb6eac36.
    
    Signed-off-by: g00857378 <guochenyang@huawei.com>

--- cgroup_sched/framework/sched_controller/include/app_state_observer.h
+++ cgroup_sched/framework/sched_controller/include/app_state_observer.h
@@ -36,7 +36,6 @@ public:
     void OnApplicationStateChanged(const AppStateData &appStateData) override;
     void OnProcessStateChanged(const ProcessData &processData) override;
     void OnAppStateChanged(const AppStateData &appStateData) override;
-    void OnAppCacheStateChanged(const AppStateData &appStateData) override;
 
 private:
     inline bool ValidateAppStateData(const AppStateData &appStateData) const
