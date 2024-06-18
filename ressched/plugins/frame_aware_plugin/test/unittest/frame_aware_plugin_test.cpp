5d1618bb4388f275fc14a836f8d262021545a195
mengyiping <1091773127@qq.com>
2024-06-18 10:10:58 +0800

    Bugfix
    Signed-off-by: mengyiping <1091773127@qq.com>

--- ressched/plugins/frame_aware_plugin/test/unittest/frame_aware_plugin_test.cpp
+++ ressched/plugins/frame_aware_plugin/test/unittest/frame_aware_plugin_test.cpp
@@ -87,6 +87,7 @@ HWTEST_F(FrameAwarePluginTest, HandleAppStateChangeTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // bundleName is not string
     data->payload["bundleName"] = "test";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 
 /**
@@ -126,6 +127,7 @@ HWTEST_F(FrameAwarePluginTest, HandleProcessStateChangeTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // bundleName is not string
     data->payload["bundleName"] = "test";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 
 /**
@@ -175,6 +177,7 @@ HWTEST_F(FrameAwarePluginTest, HandleCgroupAdjusterTest, TestSize.Level1)
     data->value = 1;
     data->payload["newGroup"] = "12345";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // data->value = 1
+    ffrt::wait();
 }
 
 /**
@@ -208,6 +211,7 @@ HWTEST_F(FrameAwarePluginTest, HandleWindowsFocusTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // uid is not string
     data->payload["uid"] = "12345";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 
 /**
@@ -242,6 +246,42 @@ HWTEST_F(FrameAwarePluginTest, HandleContinuousTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // uid is not string
     data->payload["uid"] = "12345";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
+}
+
+/**
+ * @tc.name: HandleEventSlide
+ * @tc.desc: Test whether HandleEventSlide interface is normal
+ * @tc.type: FUNC
+ */
+HWTEST_F(FrameAwarePluginTest, HandleEventSlideTest, TestSize.Level1)
+{
+    uint32_t type = 0;
+    int64_t value = 0;
+    nlohmann::json payload;
+    nlohmann::json payloadarray;
+    payload["test"] = false;
+    payloadarray[0] = false;
+    std::shared_ptr<ResData> data = std::make_shared<ResData>(type, value, payload);
+    std::shared_ptr<ResData> data1 = std::make_shared<ResData>(type, value, payloadarray);
+    data->resType = RES_TYPE_SLIDE_RECOGNIZE;
+    data->value = 0;
+    data1->resType = RES_TYPE_SLIDE_RECOGNIZE;
+    data1->value = 0;
+
+    FrameAwarePlugin::GetInstance().DispatchResource(data1); // not object
+    FrameAwarePlugin::GetInstance().DispatchResource(data); // not contain pid
+    data->payload.clear();
+    data->value = 0;
+    data->payload["pid"] = true;
+    FrameAwarePlugin::GetInstance().DispatchResource(data); // not contain uid
+    data->payload["uid"] = true;
+    FrameAwarePlugin::GetInstance().DispatchResource(data); // pid is not string
+    data->payload["pid"] = "12345";
+    FrameAwarePlugin::GetInstance().DispatchResource(data); // uid is not string
+    data->payload["uid"] = "12345";
+    FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 
 /**
@@ -275,6 +315,7 @@ HWTEST_F(FrameAwarePluginTest, HandleReportRenderTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // uid is not string
     data->payload["uid"] = "12345";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 
 /**
@@ -308,6 +349,7 @@ HWTEST_F(FrameAwarePluginTest, HandleNetworkLatencyRequestTest, TestSize.Level1)
     FrameAwarePlugin::GetInstance().DispatchResource(data); // identity is not string
     data->payload["identity"] = "12345";
     FrameAwarePlugin::GetInstance().DispatchResource(data); // all right
+    ffrt::wait();
 }
 } // namespace ResourceSchedule
 } // namespace OHOS
