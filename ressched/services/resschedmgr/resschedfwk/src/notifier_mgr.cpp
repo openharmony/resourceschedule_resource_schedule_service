4a6b3dfa74240aa4cf00e1956425f163d9e87562
unknown <baiheng5@huawei.com>
2024-07-06 16:11:01 +0800

    add
    Signed-off-by: unknown <baiheng5@huawei.com>

--- ressched/services/resschedmgr/resschedfwk/src/notifier_mgr.cpp
+++ ressched/services/resschedmgr/resschedfwk/src/notifier_mgr.cpp
@@ -67,7 +67,7 @@ void NotifierMgr::Init()
         return;
     }
     notifierDeathRecipient_ = sptr<IRemoteObject::DeathRecipient>(
-        new (std::nothrow) ResSchedNotifierDeathRecipient([](const sptr<IRemoteObject>& notifier){
+        new (std::nothrow) ResSchedNotifierDeathRecipient([](const sptr<IRemoteObject>& notifier) {
             NotifierMgr::GetInstance().OnRemoteNotifierDied(notifier);
         }));
     notifierHandler_ = std::make_shared<ffrt::queue>("DeviceNotifyQueue");
