b8c601d87beac58668366c56b53c4fc080f4da95
qq469587496 <quyuhang2@huawei.com>
2024-07-09 17:11:08 +0800

    修改读取proc文件执行逻辑
    Signed-off-by: qq469587496 <quyuhang2@huawei.com>

--- cgroup_sched/framework/utils/ressched_utils.cpp
+++ cgroup_sched/framework/utils/ressched_utils.cpp
@@ -182,15 +182,16 @@ void ResSchedUtils::DispatchResourceExt(uint32_t resType, int64_t value, const n
 bool ResSchedUtils::CheckTidIsInPid(int32_t pid, int32_t tid)
 {
     nlohmann::json payload;
-    payload["pid"] = tid;
+    payload["pid"] = pid;
+    payload["tid"] = tid;
     nlohmann::json reply;
     ResourceSchedule::ResSchedExeClient::GetInstance().SendRequestSync(
         ResExeType::RES_TYPE_CGROUP_PROC_TASK_SYNC_EVENT, 0, payload, reply);
     std::string resStr{"res"};
-    if (!reply.contains(resStr) || !reply[resStr].is_string()) {
+    if (!reply.contains(resStr) || !reply[resStr].is_boolean()) {
         return false;
     }
-    return reply[resStr].get<bool>();
+    return reply[resStr];
 }
 
 void ResSchedUtils::ReportAppStateInProcess(int32_t state, int32_t pid)
