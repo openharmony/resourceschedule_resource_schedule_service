d45b77234d4e62b420cb256a4d7808f031de6efe
qq469587496 <quyuhang2@huawei.com>
2024-07-09 20:44:03 +0800

    修改读取proc文件执行逻辑
    Signed-off-by: qq469587496 <quyuhang2@huawei.com>

--- ressched_executor/services/resschedexemgr/src/res_sched_exe_mgr.cpp
+++ ressched_executor/services/resschedexemgr/src/res_sched_exe_mgr.cpp
@@ -137,7 +137,7 @@ void ResSchedExeMgr::HandleRequestForCgroup(uint32_t resType, const nlohmann::js
         case ResExeType::RES_TYPE_CGROUP_SYNC_EVENT: 
             GetCgroupFileContent(resType, payload, reply);
             break;
-        case ResExeType::RES_TYPE_CGROUP_SYNC_EVENT:
+        case ResExeType::RES_TYPE_CGROUP_PROC_TASK_SYNC_EVENT:
             CheckProcTaskForCgroup(resType, payload, reply);
             break;
         default:
