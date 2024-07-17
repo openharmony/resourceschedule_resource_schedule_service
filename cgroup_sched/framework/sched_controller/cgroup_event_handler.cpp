dcab6fb3e8722fe9f48966c5ffb18c189aec6c6c
superhanxin <hanxin27@huawei.com>
2024-07-17 15:22:37 +0800

    补充进程创建时分组的维测信息
    Signed-off-by: superhanxin <hanxin27@huawei.com>

--- cgroup_sched/framework/sched_controller/cgroup_event_handler.cpp
+++ cgroup_sched/framework/sched_controller/cgroup_event_handler.cpp
@@ -262,8 +262,8 @@ void CgroupEventHandler::HandleProcessCreated(uid_t uid, pid_t pid, int32_t host
         CGS_LOGE("%{public}s : supervisor nullptr!", __func__);
         return;
     }
-    CGS_LOGD("%{public}s : %{public}d, %{public}d, %{public}d, %{public}s", __func__, uid, pid, hostPid,
-        bundleName.c_str());
+    CGS_LOGD("%{public}s : %{public}d, %{public}d, %{public}d, %{public}d, %{public}s, %{public}d",
+        __func__, uid, pid, hostPid, processType, bundleName.c_str(), extensionType);
     ChronoScope cs("HandleProcessCreated");
     std::shared_ptr<Application> app = supervisor_->GetAppRecordNonNull(uid);
     std::shared_ptr<ProcessRecord> procRecord = app->GetProcessRecordNonNull(pid);
