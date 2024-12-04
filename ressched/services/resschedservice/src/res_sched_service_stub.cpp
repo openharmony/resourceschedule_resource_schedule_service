2a0be67f2260270ce85e1a6269c218b7c9558d17
tongyi <tongyi7@huawei.com>
2024-12-04 01:54:24 +0000

    服务端上报音频流进入standby事件
    Signed-off-by: tongyi <tongyi7@huawei.com>

--- ressched/services/resschedservice/src/res_sched_service_stub.cpp
+++ ressched/services/resschedservice/src/res_sched_service_stub.cpp
@@ -161,6 +161,7 @@ namespace {
         ResType::RES_TYPE_DEVICE_IDLE,
         ResType::RES_TYPE_BT_SERVICE_EVENT,
         ResType::SYNC_RES_TYPE_GET_NWEB_PRELOAD_SET,
+        ResType::RES_TYPE_AUDIO_RENDERER_STANDBY,
     };
     const std::string NEEDED_PERMISSION = "ohos.permission.REPORT_RESOURCE_SCHEDULE_EVENT";
     const std::string SCENEBOARD_BUNDLE_NAME = "com.ohos.sceneboard";
