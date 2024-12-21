bc0bf64f98888820c8fa02ea70aac4105b956a6c
tongyi <tongyi7@huawei.com>
2024-12-21 06:32:01 +0000

    update ressched/interfaces/innerkits/ressched_client/include/res_type.h.
    Signed-off-by: tongyi <tongyi7@huawei.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -235,6 +235,8 @@ enum : uint32_t {
     RES_TYPE_ONLY_PERF_APP_COLD_START = 104,
     // report scene rotation, value 0 start, value 1 stop
     RES_TYPE_SCENE_ROTATION = 105,
+    // report event vsync tid, value tid, payload pid, tid, uid.
+    RES_TYPE_REPORT_VSYNC_TID = 106,
     // report cloud config update
     RES_TYPE_CLOUD_CONFIG_UPDATE = 107,
     // system ability status change
