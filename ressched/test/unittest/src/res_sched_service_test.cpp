7318c2956f22f9b950377d983d1c695bec12ae71
chengshichang <chengshichang@huawei.com>
2024-12-27 15:55:22 +0800

    Signed-off-by: csc<chengshichang@huawei.com>

--- ressched/test/unittest/src/res_sched_service_test.cpp
+++ ressched/test/unittest/src/res_sched_service_test.cpp
@@ -802,13 +802,13 @@ HWTEST_F(ResSchedServiceTest, IsAllowedAppPreloadInner001, Function | MediumTest
  * @tc.require: issueIBE0PK
  * @tc.author:csc
  */
-HWTEST_F(ResSchedServiceTest, IsAllowedAppPreloadInner001, Function | MediumTest | Level0)
+HWTEST_F(ResSchedServiceTest, IsAllowedLinkJumpInner001, Function | MediumTest | Level0)
 {
     auto resSchedServiceStub_ = make_shared<TestResSchedServiceStub>();
     resSchedServiceStub_->Init();
     MessageParcel reply;
     MessageParcel emptyData;
-    int32_t ret = resSchedServiceStub_->IsAllowedAppPreloadInner(emptyData, reply);
+    int32_t ret = resSchedServiceStub_->IsAllowedLinkJumpInner(emptyData, reply);
     EXPECT_NE(ret, ERR_OK);
 }
 
