47fe62fdc038e83c9c24ad95e67e175850dddcdb
houjiahui <houjiahui2@h-partners.com>
2024-12-28 09:00:15 +0800

    Complete the permission to report game event.
    Signed-off-by: houjiahui <houjiahui2@h-partners.com>

--- ressched/services/resschedservice/src/res_sched_service_stub.cpp
+++ ressched/services/resschedservice/src/res_sched_service_stub.cpp
@@ -159,6 +159,7 @@ namespace {
         ResType::RES_TYPE_APP_GAME_BOOST_EVENT,
         ResType::RES_TYPE_DISPLAY_MULTI_SCREEN,
         ResType::RES_TYPE_INTENT_CTRL_APP,
+        ResType::RES_TYPE_GET_GAME_SCENE_INFO,
     };
     const std::string NEEDED_PERMISSION = "ohos.permission.REPORT_RESOURCE_SCHEDULE_EVENT";
     const std::string SCENEBOARD_BUNDLE_NAME = "com.ohos.sceneboard";
