4d9cf61c86f6dc1eb397da51e0f8cd7b27cd4c0a
unknown <baiheng5@huawei.com>
2025-01-15 09:31:48 +0800

    增加抢红包事件
    Signed-off-by: unknown <baiheng5@huawei.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -281,6 +281,8 @@ enum : uint32_t {
     RES_TYPE_INTENT_CTRL_APP = 126,
     // crown rotation value 0:start, value 2:end
     RES_TYPE_CROWN_ROTATION_STATUS = 129,
+    // report red envelope scene
+    RES_TYPE_RED_ENVELOPE = 130 ,
     // last async resType
     ASYNC_RES_TYPE_LAST,
     // first sync resType
