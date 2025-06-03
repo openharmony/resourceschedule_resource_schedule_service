2997e5716be815f282cd133f6a5ffb936efaea3a
xxx <xiaozhiwen7@h-partners.com>
2025-06-03 01:56:14 +0000

    update ressched/interfaces/innerkits/ressched_client/include/res_type.h.
    Signed-off-by: xxx <xiaozhiwen7@h-partners.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -343,8 +343,10 @@ enum : uint32_t {
     RES_TYPE_SWIPE_DIRECTION_UP = 159,
     // report window panel, value 0: minimize, value 1: maxmize, value 2: recovery
     RES_TYPE_WINDOW_PANEL = 160,
+    // report boker gatt connect
+    RES_TYPE_REPORT_BOKER_GATT_CONNECT = 161,
     // report main screen split，value 0 enter scene 1 exit scene.
-    RES_TYPE_WINDOW_SPLIT_SCREEN = 161,
+    RES_TYPE_WINDOW_SPLIT_SCREEN = 162,
     // last async resType
     ASYNC_RES_TYPE_LAST,
     // first sync resType
