4e1e056b36474f2e63df732fbdba96abe6a8cbfb
wangz <wangzhen350@huawei.com>
2025-06-07 11:52:40 +0800

    代码优化
    Signed-off-by: wangz <wangzhen350@huawei.com>

--- ressched/sched_controller/observer/include/window_state_observer.h
+++ ressched/sched_controller/observer/include/window_state_observer.h
@@ -18,8 +18,71 @@
 
 #include "window_manager_lite.h"
 
+#include <sys/types.h>
+#include "iremote_object.h"
+#include "window_manager.h"
+#include "wm_common.h"
+#include "nlohmann/json.hpp"
+#include <cstdint>
+
 namespace OHOS {
 namespace ResourceSchedule {
+
+namespace RSSWindowMode {
+    enum:uint8_t {
+        // The frist bit represents the floating bit,0 to enter, 1 to exit
+        // The second bit represents the split bit,0 to enter, 1 to exit
+        WINDOW_MODE_SPLIT_FLOATING = 0b00,
+        WINDOW_MODE_SPLIT = 0b01,
+        WINDOW_MODE_FLOATING = 0b10,
+        WINDOW_MODE_OTHER = 0b11,
+
+        // The frist bit represents the floating bit,0 to changed, 1 to unchanged
+        // The second bit represents the split bit,0 to changed, 1 to unchanged
+        WINDOW_MODE_SPLIT_FLOATING_UNCHANGED = 0b00,
+        WINDOW_MODE_FLOATING_CHANGED = 0b01,
+        WINDOW_MODE_SPLIT_CHANGED = 0b10,
+        WINDOW_MODE_SPLIT_FLOATING_CHANGED = 0b11,
+    };
+}
+using OHOS::Rosen::IFocusChangedListener;
+using OHOS::Rosen::IVisibilityChangedListener;
+using OHOS::Rosen::IDrawingContentChangedListener;
+using OHOS::Rosen::IWindowModeChangedListener;
+using OHOS::Rosen::WindowType;
+using OHOS::Rosen::FocusChangeInfo;
+using OHOS::Rosen::WindowVisibilityInfo;
+using OHOS::Rosen::WindowDrawingContentInfo;
+using OHOS::Rosen::WindowModeType;
+
+class WindowStateObserver : public IFocusChangedListener {
+public:
+    void OnFocused(const sptr<FocusChangeInfo>& focusChangeInfo) override;
+    void OnUnfocused(const sptr<FocusChangeInfo>& focusChangeInfo) override;
+};
+
+class WindowVisibilityObserver : public IVisibilityChangedListener {
+public:
+    void OnWindowVisibilityChanged(const std::vector<sptr<WindowVisibilityInfo>>& windowVisibilityInfo) override;
+
+private:
+    void MarshallingWindowVisibilityInfo(const sptr<WindowVisibilityInfo>& info, nlohmann::json& payload);
+};
+
+class WindowDrawingContentObserver : public IDrawingContentChangedListener {
+public:
+    void OnWindowDrawingContentChanged(const std::vector<sptr<WindowDrawingContentInfo>>& changeInfo) override;
+
+private:
+    void MarshallingWindowDrawingContentInfo(const sptr<WindowDrawingContentInfo>& info, nlohmann::json& payload);
+};
+class WindowModeObserver : public IWindowModeChangedListener {
+public:
+    void OnWindowModeUpdate(const WindowModeType mode) override;
+private:
+    uint8_t MarshallingWindowModeType(const WindowModeType mode);
+    uint8_t lastWindowMode_ = RSSWindowMode::WINDOW_MODE_OTHER;
+};
 
 class PiPStateObserver : public OHOS::Rosen::IPiPStateChangedListener {
     public:
--- ressched/sched_controller/observer/src/window_state_observer.cpp
+++ ressched/sched_controller/observer/src/window_state_observer.cpp
@@ -19,8 +19,201 @@
 #include "res_sched_mgr.h"
 #include "res_type.h"
 
+#include "cgroup_sched_log.h"
+#include "cgroup_event_handler.h"
+#include "res_type.h"
+#include "sched_controller.h"
+#include "ressched_utils.h"
+
 namespace OHOS {
 namespace ResourceSchedule {
+
+const uint8_t WINDOW_MODE_SPLIT_BIT = 1;
+const uint8_t WINDOW_MODE_BIT_EXIT = 1;
+
+void WindowStateObserver::OnFocused(const sptr<FocusChangeInfo>& focusChangeInfo)
+{
+    if (!focusChangeInfo) {
+        return;
+    }
+
+    auto cgHandler = SchedController::GetInstance().GetCgroupEventHandler();
+    if (cgHandler) {
+        auto windowId = focusChangeInfo->windowId_;
+        auto windowType = focusChangeInfo->windowType_;
+        auto displayId = focusChangeInfo->displayId_;
+        auto pid = focusChangeInfo->pid_;
+        auto uid = focusChangeInfo->uid_;
+
+        cgHandler->PostTask([cgHandler, windowId, windowType, displayId, pid, uid] {
+            cgHandler->HandleFocusedWindow(windowId, windowType, displayId, pid, uid);
+        });
+    }
+
+    nlohmann::json payload;
+    payload["pid"] = std::to_string(focusChangeInfo->pid_);
+    payload["uid"] = std::to_string(focusChangeInfo->uid_);
+    payload["windowId"] = std::to_string(focusChangeInfo->windowId_);
+    payload["windowType"] = std::to_string((int32_t)(focusChangeInfo->windowType_));
+    payload["displayId"] = std::to_string(focusChangeInfo->displayId_);
+    ResSchedUtils::GetInstance().ReportDataInProcess(
+        ResType::RES_TYPE_WINDOW_FOCUS, ResType::WindowFocusStatus::WINDOW_FOCUS, payload);
+}
+
+void WindowStateObserver::OnUnfocused(const sptr<FocusChangeInfo>& focusChangeInfo)
+{
+    if (!focusChangeInfo) {
+        return;
+    }
+
+    auto cgHandler = SchedController::GetInstance().GetCgroupEventHandler();
+    if (cgHandler) {
+        auto windowId = focusChangeInfo->windowId_;
+        auto windowType = focusChangeInfo->windowType_;
+        auto displayId = focusChangeInfo->displayId_;
+        auto pid = focusChangeInfo->pid_;
+        auto uid = focusChangeInfo->uid_;
+
+        cgHandler->PostTask([cgHandler, windowId, windowType, displayId, pid, uid] {
+            cgHandler->HandleUnfocusedWindow(windowId, windowType, displayId, pid, uid);
+        });
+    }
+
+    nlohmann::json payload;
+    payload["pid"] = std::to_string(focusChangeInfo->pid_);
+    payload["uid"] = std::to_string(focusChangeInfo->uid_);
+    payload["windowId"] = std::to_string(focusChangeInfo->windowId_);
+    payload["windowType"] = std::to_string((int32_t)(focusChangeInfo->windowType_));
+    payload["displayId"] = std::to_string(focusChangeInfo->displayId_);
+    ResSchedUtils::GetInstance().ReportDataInProcess(
+        ResType::RES_TYPE_WINDOW_FOCUS, ResType::WindowFocusStatus::WINDOW_UNFOCUS, payload);
+}
+
+void WindowVisibilityObserver::MarshallingWindowVisibilityInfo(const sptr<WindowVisibilityInfo>& info,
+    nlohmann::json& payload)
+{
+    bool isVisible = info->visibilityState_ < Rosen::WindowVisibilityState::WINDOW_VISIBILITY_STATE_TOTALLY_OCCUSION;
+    payload["pid"] = std::to_string(info->pid_);
+    payload["uid"] = std::to_string(info->uid_);
+    payload["windowId"] = std::to_string(info->windowId_);
+    payload["windowType"] = std::to_string((int32_t)info->windowType_);
+    payload["isVisible"] = isVisible;
+    payload["visibilityState"] = std::to_string(info->visibilityState_);
+}
+
+void WindowVisibilityObserver::OnWindowVisibilityChanged(
+    const std::vector<sptr<WindowVisibilityInfo>>& windowVisibilityInfo)
+{
+    auto cgHandler = SchedController::GetInstance().GetCgroupEventHandler();
+    if (!cgHandler) {
+        return;
+    }
+    for (auto& info : windowVisibilityInfo) {
+        if (!info) {
+            continue;
+        }
+        auto windowId = info->windowId_;
+        auto visibilityState = info->visibilityState_;
+        auto windowType = info->windowType_;
+        auto pid = info->pid_;
+        auto uid = info->uid_;
+        cgHandler->PostTask([cgHandler, windowId, visibilityState, windowType, pid, uid] {
+            cgHandler->HandleWindowVisibilityChanged(windowId, visibilityState, windowType, pid, uid);
+        });
+        nlohmann::json payload;
+        MarshallingWindowVisibilityInfo(info, payload);
+        bool isVisible = visibilityState < Rosen::WindowVisibilityState::WINDOW_VISIBILITY_STATE_TOTALLY_OCCUSION;
+        ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_WINDOW_VISIBILITY_CHANGE,
+            isVisible ? ResType::WindowVisibilityStatus::VISIBLE : ResType::WindowVisibilityStatus::INVISIBLE, payload);
+    }
+}
+
+void WindowDrawingContentObserver::MarshallingWindowDrawingContentInfo(const sptr<WindowDrawingContentInfo>& info,
+    nlohmann::json& payload)
+{
+    payload["pid"] = std::to_string(info->pid_);
+    payload["uid"] = std::to_string(info->uid_);
+    payload["windowId"] = std::to_string(info->windowId_);
+    payload["windowType"] = std::to_string((int32_t)info->windowType_);
+    payload["drawingContentState"] = info->drawingContentState_;
+}
+
+void WindowDrawingContentObserver::OnWindowDrawingContentChanged(
+    const std::vector<sptr<WindowDrawingContentInfo>>& changeInfo)
+{
+    auto cgHandler = SchedController::GetInstance().GetCgroupEventHandler();
+    if (!cgHandler) {
+        return;
+    }
+    for (const auto& info : changeInfo) {
+        if (!info) {
+            continue;
+        }
+        auto windowId = info->windowId_;
+        auto windowType = info->windowType_;
+        auto drawingContentState = info->drawingContentState_;
+        auto pid = info->pid_;
+        auto uid = info->uid_;
+        cgHandler->PostTask([cgHandler, windowId, windowType, drawingContentState, pid, uid] {
+            cgHandler->HandleDrawingContentChangeWindow(windowId, windowType, drawingContentState, pid, uid);
+        });
+        nlohmann::json payload;
+        MarshallingWindowDrawingContentInfo(info, payload);
+        ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_WINDOW_DRAWING_CONTENT_CHANGE,
+            drawingContentState ? ResType::WindowDrawingStatus::Drawing : ResType::WindowDrawingStatus::NotDrawing,
+            payload);
+    }
+}
+void WindowModeObserver::OnWindowModeUpdate(const WindowModeType mode)
+{
+    CGS_LOGI("WindowModeObserver OnWindowModeUpdate mode: %{public}hhu ", mode);
+    uint8_t nowWindowMode = MarshallingWindowModeType(mode);
+    uint8_t windowModeChangeBit = nowWindowMode ^ lastWindowMode_;
+    nlohmann::json payload;
+    uint8_t windowModeSplitValue = (nowWindowMode >> WINDOW_MODE_SPLIT_BIT) & WINDOW_MODE_BIT_EXIT;
+    uint8_t windowModeFloatingValue = nowWindowMode & WINDOW_MODE_BIT_EXIT;
+    switch (windowModeChangeBit) {
+        case RSSWindowMode::WINDOW_MODE_FLOATING_CHANGED:
+            ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_FLOATING_WINDOW,
+                windowModeFloatingValue, payload);
+            break;
+        case RSSWindowMode::WINDOW_MODE_SPLIT_CHANGED:
+            ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_SPLIT_SCREEN,
+                windowModeSplitValue, payload);
+            break;
+        case RSSWindowMode::WINDOW_MODE_SPLIT_FLOATING_CHANGED:
+            ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_SPLIT_SCREEN,
+                windowModeSplitValue, payload);
+            ResSchedUtils::GetInstance().ReportDataInProcess(ResType::RES_TYPE_FLOATING_WINDOW,
+                windowModeFloatingValue, payload);
+            break;
+        default:
+            break;
+    }
+    lastWindowMode_ = nowWindowMode;
+}
+
+uint8_t WindowModeObserver::MarshallingWindowModeType(const WindowModeType mode)
+{
+    uint8_t nowWindowMode = RSSWindowMode::WINDOW_MODE_OTHER;
+    switch (mode) {
+        case Rosen::WindowModeType::WINDOW_MODE_SPLIT:
+            nowWindowMode = RSSWindowMode::WINDOW_MODE_SPLIT;
+            break;
+        case Rosen::WindowModeType::WINDOW_MODE_FLOATING:
+        case Rosen::WindowModeType::WINDOW_MODE_FULLSCREEN_FLOATING:
+            nowWindowMode = RSSWindowMode::WINDOW_MODE_FLOATING;
+            break;
+        case Rosen::WindowModeType::WINDOW_MODE_SPLIT_FLOATING:
+            nowWindowMode = RSSWindowMode::WINDOW_MODE_SPLIT_FLOATING;
+            break;
+        default:
+            nowWindowMode = RSSWindowMode::WINDOW_MODE_OTHER;
+            break;
+    }
+    return nowWindowMode;
+}
+
 void PiPStateObserver::OnPiPStateChanged(const std::string& bundleName, const bool isForeground)
 {
     RESSCHED_LOGI("Receive OnPiPStateChange %{public}s %{public}d", bundleName.c_str(), isForeground);
