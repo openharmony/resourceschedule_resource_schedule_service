8a199d604095aff91f8ea5d14291e88876bace23
fengj130406 <fengj130406@outlook.com>
2025-09-01 16:38:24 +0800

    camera report event
    Signed-off-by: fengj130406 <fengj130406@outlook.com>

--- ressched/services/resschedservice/src/res_sched_service.cpp
+++ ressched/services/resschedservice/src/res_sched_service.cpp
@@ -168,6 +168,7 @@ namespace {
         ResType::RES_TYPE_TDP_TURBO,
         ResType::SYNC_RES_TYPE_QUERY_TOPN_BUNDLE_USAGE_INFO,
         ResType::RES_TYPE_BACKGROUND_STATUS,
+        ResType::RES_TYPE_CAMERA_STATUS_CHANGED,
     };
     static const std::unordered_map<uint32_t, std::unordered_set<int32_t>> ALLOW_SOME_UID_REPORT_RES = {
         { ResType::RES_TYPE_MODEM_PA_HIGH_POWER_ABNORMAL, { 1201 } },
