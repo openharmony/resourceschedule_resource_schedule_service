8b8200342e7f8d8883c67e83efc929fa446de574
xufu <xufu7@huawei.com>
2025-11-13 16:50:55 +0800

    recv media ctrl event
    Signed-off-by: xufu <xufu7@huawei.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -371,6 +371,8 @@ enum : uint32_t {
     RES_TYPE_PROCESS_PRE_FOREGROUND_CHANGE = 173,
     // Turbo Mode
     RES_TYPE_TDP_TURBO = 174,
+    // media ctrl playback event
+    RES_TYPE_MEDIA_CTRL_EVENT = 189,
     // last async resType
     ASYNC_RES_TYPE_LAST,
     // first sync resType
