63bee16896e47d90ca541f93140e111a9faa22e8
buzhi_ <wangzhen350@huawei.com>
2025-11-15 13:32:44 +0800

    添加后台拉起消息
    Signed-off-by: buzhi_ <wangzhen350@huawei.com>

--- ressched/services/resschedservice/src/res_sched_service.cpp
+++ ressched/services/resschedservice/src/res_sched_service.cpp
@@ -176,6 +176,7 @@ namespace {
         ResType::RES_TYPE_CODEC_ENCODE_STATUS_CHANGED,
         ResType::RES_TYPE_CAMERA_LENS_STATUS_CHANGED,
         ResType::RES_TYPE_START_INPUT_METHOD_PROCESS,
+        ResType::RES_TYPE_SA_PULL_APP_IDENTIFIER,
     };
     static const std::unordered_map<uint32_t, std::unordered_set<int32_t>> ALLOW_SOME_UID_REPORT_RES = {
         { ResType::RES_TYPE_MODEM_PA_HIGH_POWER_ABNORMAL, { 1201 } },
