d0e8042f6807a4795b3732714a7eb69807921d94
liuyuqing_35 <liuyuqing35@h-partners.com>
2026-06-10 15:46:37 +0800

    新增接续事件监听
    Signed-off-by: liuyuqing_35 <liuyuqing35@h-partners.com>

--- ressched/interfaces/innerkits/ressched_client/include/res_type.h
+++ ressched/interfaces/innerkits/ressched_client/include/res_type.h
@@ -445,6 +445,8 @@ enum : uint32_t {
     RES_TYPE_LOAD_KILL_REASON_CONFIG = 214,
     // report battery low
     RES_TYPE_BATTERY_LOW = 215,
+    // report continuation event
+    RES_TYPE_DISTRIBUTED_CONTINUATION_EVENT = 217,
     // last async resType
     ASYNC_RES_TYPE_LAST,
     // first sync resType
